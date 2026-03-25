e0ec968ca9cf4fafd9561d26aeaf3efd1a4740a8
alissoncarrdoso-coder <alisson.carrdoso@gmail.com>
2026-03-25 18:33:17 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE=‘produtividade-v1’;
+self.addEventListener(‘install’,e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll([’/’,’/index.html’])).then(()=>self.skipWaiting())));
+self.addEventListener(‘activate’,e=>e.waitUntil(self.clients.claim()));
+self.addEventListener(‘fetch’,e=>{if(e.request.method!==‘GET’)return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request)))});
